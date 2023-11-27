d2c4a1fad0922b3d3b39f807fda264426d2ddcd6
Gordana Ilic <gordanailic171@gmail.com>
2023-11-27 12:29:50 +0100

    aa

--- app/src/main/java/com/example/cocktails/MainActivity.kt
+++ app/src/main/java/com/example/cocktails/MainActivity.kt
@@ -7,5 +7,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }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
+    }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fasdas
 }
