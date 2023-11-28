a50323102eebfa1964ea32e59f11b81fcaae48b4
Gordana Ilic <>
2023-11-28 14:28:30 +0100

    Doradjen splash i obrisane nepotrebne klase

--- app/src/main/java/com/example/cocktails/SplashScreenActivity.kt
+++ app/src/main/java/com/example/cocktails/SplashScreenActivity.kt
@@ -2,19 +2,24 @@ package com.example.cocktails
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.annotation.RequiresApi
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class SplashScreenActivity : AppCompatActivity() {
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            installSplashScreen()
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_splash_screen)
-            val intent = Intent(this, Helper::class.java)
-            startActivity(intent)
+    private var keepSplashOnScreen = true
+    private val delay = 2000L
 
-        }
-
+    @RequiresApi(Build.VERSION_CODES.S)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen().setKeepOnScreenCondition { keepSplashOnScreen }
+        Handler(Looper.getMainLooper()).postDelayed({ keepSplashOnScreen = false }, delay)
+        setContentView(R.layout.activity_splash_screen)
+    }
 }
