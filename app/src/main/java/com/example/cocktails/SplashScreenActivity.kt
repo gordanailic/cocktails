18b56f884d6387ae5f9dff9373f614f07d608ada
Gordana Ilic <>
2023-11-29 09:42:12 +0100

    Add Bottom Navigation Bar

--- app/src/main/java/com/example/cocktails/SplashScreenActivity.kt
+++ app/src/main/java/com/example/cocktails/SplashScreenActivity.kt
@@ -1,6 +1,7 @@
 package com.example.cocktails
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -9,9 +10,16 @@ import android.os.Handler
 import android.os.Looper
 import androidx.annotation.RequiresApi
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.cocktails.databinding.ActivitySplashScreenBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class SplashScreenActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivitySplashScreenBinding
     private var keepSplashOnScreen = true
     private val delay = 2000L
 
@@ -20,6 +28,25 @@ class SplashScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         installSplashScreen().setKeepOnScreenCondition { keepSplashOnScreen }
         Handler(Looper.getMainLooper()).postDelayed({ keepSplashOnScreen = false }, delay)
-        setContentView(R.layout.activity_splash_screen)
+      //  setContentView(R.layout.activity_splash_screen)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
     }
+    private fun init() {
+        initUI()
+    }
+
+    private fun initUI() {
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+        val bottomNavigationView = binding.bottomNavigationView
+        val navController = findNavController(R.id.fragmentContainerView)
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.cocktailsFragment,
+                                                            R.id.favoritesFragment,
+                                                            R.id.profileFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        bottomNavigationView.setupWithNavController(navController)
+    }
+
 }
