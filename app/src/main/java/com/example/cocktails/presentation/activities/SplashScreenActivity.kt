1b88663df09db0e852160a8263ad8c50e33d5dee
Gordana Ilic <>
2023-12-18 11:16:39 +0100

    Create Authentication Activity

--- app/src/main/java/com/example/cocktails/presentation/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/example/cocktails/presentation/activities/SplashScreenActivity.kt
@@ -2,11 +2,8 @@ package com.example.cocktails.presentation.activities
 
 import android.os.Build
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -19,14 +16,11 @@ import dagger.hilt.android.AndroidEntryPoint
 class SplashScreenActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySplashScreenBinding
-    private var keepSplashOnScreen = true
-    private val delay = 2000L
+
 
     @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        installSplashScreen().setKeepOnScreenCondition { keepSplashOnScreen }
-        Handler(Looper.getMainLooper()).postDelayed({ keepSplashOnScreen = false }, delay)
         binding = ActivitySplashScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initUI()
---
+++ app/src/main/java/com/example/cocktails/presentation/fragments/LoginFragment.kt
@@ -0,0 +1,23 @@
+package com.example.cocktails.presentation.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.cocktails.R
+import com.example.cocktails.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
